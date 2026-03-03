bfaf4c1615dcdf189b6e8b00e788e6f2df09dc6e
Levi Nicola Santos <levi.santos@timeware.com.br>
2026-03-03 12:06:21 -0300

    criadas services do projeto

--- Unievent.Domain/Entities/EntidadeBase.cs
+++ Unievent.Domain/Entities/EntidadeBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Unievent.Domain.Entities
+﻿namespace Unievent.Domain.Entities
 {
     public class EntidadeBase
     {
--- Unievent.Domain/Entities/Evento.cs
+++ Unievent.Domain/Entities/Evento.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Unievent.Domain.Entities
+﻿namespace Unievent.Domain.Entities
 {
     public class Evento : EntidadeBase
     {
